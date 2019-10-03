f1734c31e8c621128a810d467a1afd0f2a63c0d0
Xtreme1-eng <55376761+Xtreme1-eng@users.noreply.github.com>
2019-10-03 09:02:28 +0100

    Add files via upload

---
+++ student.js
@@ -0,0 +1,16 @@
+console.log('STUDENT');
+
+let surname = "Ajayi";
+console.log(surname);
+
+let otherNames = 'Victor Oluwalowu';
+console.log(otherNames);
+
+let course = 'Religions';
+console.log(course);
+
+let level = '300L';
+console.log(level);
+
+let matricNumber = '17/15ch019';
+console.log(matricNumber);
